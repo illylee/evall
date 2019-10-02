7ad696d0c3362282429707205ece1e74d14fd8ab
kyoungjin.lee <kyoungjin.lee@pulmuone.com>
2019-10-02 10:38:09 +0900

    kakao map test

--- evall-pweb/src/main/java/com/evall/pweb/EvAllPwebApplication.java
+++ evall-pweb/src/main/java/com/evall/pweb/EvAllPwebApplication.java
@@ -3,17 +3,9 @@ package com.evall.pweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@SpringBootApplication(scanBasePackages = { "com.evall.pweb" })
+@SpringBootApplication(scanBasePackages = { "com.evall" })
 public class EvAllPwebApplication extends SpringBootServletInitializer {
-
-    @RequestMapping("/")
-    public String home(){
-        return "hello, EV ALL pweb";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(EvAllPwebApplication.class, args);
