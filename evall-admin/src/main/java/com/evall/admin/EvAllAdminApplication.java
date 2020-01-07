166e4bde7f8380de4d8808effd42f042f92b695e
kyoungjin.lee <kyoungjin.lee@pulmuone.com>
2020-01-07 14:38:24 +0900

    pkg 정리

--- evall-admin/src/main/java/com/evall/admin/EvAllAdminApplication.java
+++ evall-admin/src/main/java/com/evall/admin/EvAllAdminApplication.java
@@ -2,18 +2,11 @@ package com.evall.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 @SpringBootApplication(scanBasePackages = { "com.evall.admin" })
-public class EvAllAdminApplication extends SpringBootServletInitializer {
-
-    @RequestMapping("/")
-    public String home(){
-        return "hello, EV ALL admin";
-    }
+public class EvAllAdminApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EvAllAdminApplication.class, args);
